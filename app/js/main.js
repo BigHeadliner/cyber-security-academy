6c4efdfcc279571e550aff1ea6c5ae570351c3b9
BigHeadliner <bigheadliner@gmail.com>
2023-08-07 01:00:10 +0300

    faq section ready

--- app/js/main.js
+++ app/js/main.js
@@ -2,7 +2,12 @@ $(function () {
      
     
  
-
+    $('.faq__question-top').on('click', function (){  
+        $(this).next('.faq__question-content').slideToggle();
+        $(this).children('.faq__action-btn').toggleClass('faq__action-btn--rotate'); 
+      })    
+       
+      $('.faq__question-content').hide();  
   
  
   
